16d5efc6df46e3033b74a42c2b9c1fbb5b9f876f
DESKTOP-2HA3E7O\jill <jillianjagoda@gmail.com>
2021-10-10 16:45:34 -0500

    updating sql

--- Analysis Project Folder/Pewlet-Hackard-Analysis Folder/Queries/Schema.sql
+++ Analysis Project Folder/Pewlet-Hackard-Analysis Folder/Queries/Schema.sql
@@ -31,3 +31,20 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
+CREATE TABLE Dept_Emp (
+  dept_no VARCHAR(4)NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE Titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
+);
